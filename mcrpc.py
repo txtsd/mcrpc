f816de36f03fa7b5ecfabfa7088276352e0a0537
txtsd <thexerothermicsclerodermoid@gmail.com>
2022-01-18 12:30:56 +0530

    Allow comparing 2 minecraft versions

--- mcrpc.py
+++ mcrpc.py
@@ -2,6 +2,7 @@ import glob
 import hashlib
 import json
 import os
+import re
 import shutil
 import sys
 from distutils.dir_util import copy_tree
@@ -10,18 +11,20 @@ from zipfile import ZipFile
 
 import requests
 
-if sys.platform.startswith('linux'):
+PLATFORM = sys.platform
+if PLATFORM.startswith('linux'):
     PATH_DOTMC = os.path.join(os.path.expanduser('~'), '.minecraft')
-elif sys.platform.startswith('darwin'):
+elif PLATFORM.startswith('darwin'):
     PATH_DOTMC = os.path.join(os.path.expanduser('~'), 'Library', 'Application Support', 'minecraft')
-elif sys.platform.startswith('win32'):
+elif PLATFORM.startswith('win32'):
     PATH_DOTMC = os.path.join(os.path.expandvars(r'%APPDATA%'), '.minecraft')
 
 PATH_RP_DIR = os.path.join(PATH_DOTMC, 'resourcepacks')
 PATH_CACHE = '.cache'
 PATH_TARGET = os.path.join(PATH_CACHE, 'target')
 PATH_DEFAULT = os.path.join(PATH_CACHE, 'default')
-PATH_CLIENT = os.path.join(PATH_CACHE, 'client.jar')
+PATH_CLIENT_LOW = os.path.join(PATH_CACHE, 'client_low.jar')
+PATH_CLIENT_HIGH = os.path.join(PATH_CACHE, 'client_high.jar')
 LINK_VM = 'https://launchermeta.mojang.com/mc/game/version_manifest_v2.json'
 BUF_SIZE = 65536
 SESSION = requests.Session()
@@ -62,12 +65,17 @@ def choose_rp():
     return(choice_rp)
 
 
-def choose_mcv():
+def choose_mcv(low=None):
     json_vm = SESSION.get(LINK_VM)
     json_vm.raise_for_status()
     json_vm = json_vm.json()
 
-    print('\nSelect the Minecraft Version you want to compare against:')
+    if low == True:
+        print('\nSelect the lower Minecraft Version:')
+    elif low == False:
+        print('\nSelect the higher Minecraft Version:')
+    else:
+        print('\nSelect the Minecraft Version you want to compare against:')
     for i, item in enumerate(json_vm['versions'], 1):
         print("  [{}] {}".format(i, item['id']))
         if i == 25:
@@ -84,29 +92,32 @@ def choose_mcv():
     return(choice_mcv)
 
 
-def setup_compare_folders(path_rp, rp):
+def setup_compare_folders(path_rp=None, mc2=False):
     if os.path.exists(PATH_TARGET):
         shutil.rmtree(PATH_TARGET)
     if os.path.exists(PATH_DEFAULT):
         shutil.rmtree(PATH_DEFAULT)
 
-    if path_rp[-4:] == ".zip":
-        with ZipFile(path_rp) as zip1:
-            for file in zip1.namelist():
-                if file.startswith('assets/'):
-                    zip1.extract(file, path=PATH_TARGET)
+    if not mc2:
+        if path_rp[-4:] == ".zip":
+            with ZipFile(path_rp) as zip1:
+                for file in zip1.namelist():
+                    if file.startswith('assets/'):
+                        zip1.extract(file, path=PATH_TARGET)
+        else:
+            copy_tree(os.path.join(path_rp, 'assets'), os.path.join(PATH_TARGET, 'assets'))
     else:
-        copy_tree(os.path.join(path_rp, 'assets'), os.path.join(PATH_TARGET, 'assets'))
+        with ZipFile(PATH_CLIENT_LOW) as zip2:
+            for file in zip2.namelist():
+                if file.startswith('assets/'):
+                    zip2.extract(file, path=PATH_TARGET)
 
-    with ZipFile(PATH_CLIENT) as zip2:
-        for file in zip2.namelist():
+    with ZipFile(PATH_CLIENT_HIGH) as zip3:
+        for file in zip3.namelist():
             if file.startswith('assets/'):
-                zip2.extract(file, path=PATH_DEFAULT)
+                zip3.extract(file, path=PATH_DEFAULT)
 
-
-def compare(rp, mcv):
-    path_rp = os.path.join(PATH_RP_DIR, rp)
-
+def get_client(mcv, low=False):
     json_version = SESSION.get(mcv['url'])
     json_version.raise_for_status()
     json_version = json_version.json()
@@ -119,8 +130,13 @@ def compare(rp, mcv):
     if not os.path.exists(PATH_CACHE):
         os.makedirs(PATH_CACHE)
 
-    if os.path.exists(PATH_CLIENT):
-        with open(PATH_CLIENT, 'rb') as f:
+    if low == True:
+        path_client = PATH_CLIENT_LOW
+    else:
+        path_client = PATH_CLIENT_HIGH
+
+    if os.path.exists(path_client):
+        with open(path_client, 'rb') as f:
             while True:
                 data = f.read(BUF_SIZE)
                 if not data:
@@ -129,24 +145,64 @@ def compare(rp, mcv):
         sha_calculated = sha1.hexdigest()
 
     if not sha_calculated == sha_client:
-        with open(PATH_CLIENT, 'wb') as f:
+        with open(path_client, 'wb') as f:
             print('Downloading client.jar…')
             download = SESSION.get(link_client)
             f.write(download.content)
 
-    setup_compare_folders(path_rp, rp)
+
+def compare(rp, mcv):
+    if type(rp) is dict:
+        # Comparing 2 minecraft versions
+        get_client(rp, low=True)
+        get_client(mcv)
+        setup_compare_folders(mc2=True)
+        pass
+    else:
+        # Comparing a resource pack against a minecraft version
+        path_rp = os.path.join(PATH_RP_DIR, rp)
+        get_client(mcv)
+        setup_compare_folders(path_rp)
+        pass
 
     dir_left = os.path.join(PATH_DEFAULT, '**', '*.*')
     dir_right = os.path.join(PATH_TARGET, '**', '*.*')
 
     files_left = glob.glob("{}".format(dir_left), recursive=True)
     files_right = glob.glob("{}".format(dir_right), recursive=True)
+
+    if PLATFORM.startswith('linux') or PLATFORM.startswith('darwin'):
+        pattern = re.compile(".*/assets/(.*)")
+    elif PLATFORM.startswith('win32'):
+        pattern = re.compile(".*\\assets\\(.*)")
+
+    for i, file in enumerate(files_left):
+        files_left[i] = pattern.sub("\g<1>", file)
+    for i, file in enumerate(files_right):
+        files_right[i] = pattern.sub("\g<1>", file)
+
     missing_files = [x for x in files_left if x not in files_right]
 
     print('\nMissing files:')
     for file in sorted(missing_files):
-        print("  {}".format(file.replace('.cache/default/assets/', '')))
+        print("  {}".format(file))
+    print('\nTotal missing files:', len(missing_files), '\n')
 
 
 if __name__ == '__main__':
-    compare(choose_rp(), choose_mcv())
+
+    while True:
+        print('\n[1] Compare a resource pack against a minecraft version')
+        print('[2] Compare a minecraft version against another minecraft version')
+        print('Choice: ', end='')
+
+        choice_logic = input()
+
+        if choice_logic == '1':
+            compare(choose_rp(), choose_mcv())
+            break
+        elif choice_logic == '2':
+            compare(choose_mcv(low=True), choose_mcv(low=False))
+            break
+        else:
+            print('\nInvalid choice!')
